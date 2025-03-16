976459dcc485174bf34f343a1b01220b8041f68a
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2025-03-16 00:16:59 -0300

    Add: Adicionando private para id e mensagem

--- skeleton-main/skeleton/skeleton-ejb/src/main/java/br/com/skeleton/business/entity/Config.java
+++ skeleton-main/skeleton/skeleton-ejb/src/main/java/br/com/skeleton/business/entity/Config.java
@@ -1,15 +1,30 @@
 package br.com.skeleton.business.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
 import java.io.Serializable;
 import java.util.Objects;
 
+
+@Entity
+@Table(name = "configs")
 public class Config implements Serializable {
 
   private static final long serialVersionUID = 6615511541532380908L;
-  private String mensagem;;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "mensagem", nullable = false)
+  private String mensagem;
 
   public Config() {}
+
 
 
   public String getMensagem() {
